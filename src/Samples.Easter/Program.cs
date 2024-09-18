07060de2c5445ae88ae0bda6710a3500a90a1154
Martin A. Ortega <lynxestudio@outlook.com>
2024-09-18 11:24:58 -0600

    Adds source code to project

---
+++ src/Samples.Easter/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Samples.Easter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, d, e, day;
+            int? year = null;
+            string syear = null;
+            Utilities.ShowMenu("Date of the Easter Sunday");
+            do
+            {
+                 syear = Utilities.Scanf("Enter a year");
+                if (syear != null)
+                {
+                    year = Int32.Parse(syear);
+                    a = year.Value % 19;
+                    b = year.Value % 4;
+                    c = year.Value % 7;
+                    d = (19 * a + 24) % 30;
+                    e = (2 * b + 4 * c + 6 * d + 5) % 7;
+                    day = 22 + d + e;
+                    string verb = "is";
+                    string msg = null;
+                    if (year != DateTime.Now.Year)
+                        verb = (year > DateTime.Now.Year ? "will be" : "was");
+                    if (day <= 31)
+                        msg = String.Format("Easter Sunday {0}, {1} on March {2}{3}", year, verb, day, Environment.NewLine);
+                    else
+                        msg = String.Format("Easter Sunday {0}, {1} on April {2}{3}", year, verb, day - 31, Environment.NewLine);
+                    Utilities.PrintMessage(msg);
+                }
+                else
+                    Utilities.PrintMessage("Year can not be null");
+
+            } while (syear != null);
+            Utilities.Pause();
+
+        }
+    }
+}
---
+++ src/Samples.Easter/Utilities.cs
@@ -0,0 +1,104 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Util.cs" company="apifiedsignature">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Samples.Easter
+{
+    using System;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    internal static class Utilities
+    {
+        internal static void ShowMenu(string title)
+        {
+            ShowMenu(title, null);
+        }
+
+        internal static void ShowMenu(String title, string[] collection)
+        {
+            Console.Clear();
+
+            int width = Console.WindowWidth - 4;
+            int i = 0;
+            Console.WriteLine("+" + new string('-', width - 2) + "+");
+            Console.WriteLine("|" + CenterText(title, width - 2) + "|");
+            Console.WriteLine("|" + new string(' ', width - 2) + "|");
+            if (collection != null)
+            {
+                foreach (var item in collection)
+                {
+                    i++;
+                    Console.WriteLine("| " + AlignLeftText(i + ")  " + item, width - 3) + "|");
+                }
+                Console.WriteLine("| " + AlignLeftText("0. Exit", width - 3) + "|");
+            }
+            Console.WriteLine("|" + new string(' ', width - 2) + "|");
+            Console.WriteLine("+" + new string('-', width - 2) + "+");
+        }
+
+        internal static void PrintLabel(string label, string value)
+        {
+            Console.WriteLine("\t{0}\t{1}",label,value);
+        }
+
+        internal static void Pause()
+        {
+            Console.WriteLine();
+            Console.WriteLine("\tPress [Enter] to continue...");
+            Console.ReadLine();
+        }
+
+        internal static void PrintMessage(string message)
+        {
+            Console.WriteLine();
+            Console.WriteLine("\t========================================\n");
+            Console.WriteLine("\t" + message);
+            Console.WriteLine("\t========================================\n");
+            Console.WriteLine();
+        }
+
+        static string CenterText(string text, int width)
+        {
+            if (text.Length >= width)
+                return text.Substring(0, width); // Truncate if text is longer than width
+
+            int spaces = width - text.Length;
+            int padLeft = spaces / 2;
+            int padRight = spaces - padLeft;
+
+            return new string(' ', padLeft) + text + new string(' ', padRight);
+        }
+
+        static string AlignLeftText(string text, int width)
+        {
+            if (text.Length >= width)
+                return text.Substring(0, width); // Truncate if text is longer than width
+
+            int spaces = width - text.Length;
+            return text + new string(' ', spaces);
+        }
+
+        internal static int? ReadLineAsInt()
+        {
+            var input = Console.ReadLine();
+            if (string.IsNullOrEmpty(input) || input.Length == 0)
+                return null;
+            else
+                return Convert.ToInt32(input);
+        }
+
+        internal static string Scanf(string message)
+        {
+            Console.Write("\t[ " + message + " ]\t");
+            string input = Console.ReadLine();
+            if (string.IsNullOrEmpty(input) || input.Length == 0)
+                return null;
+            return input;
+        }
+
+    }
+}
